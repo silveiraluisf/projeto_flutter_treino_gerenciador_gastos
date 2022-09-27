ebf501bdfb1fd0d524b9fd5a01750235a855bb56
silveiraluisf <silveiraluis88@gmail.com>
2022-09-27 14:49:18 -0300

    estilização da página e adicionado os controllers dos campos

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,9 @@ class MyHomePage extends StatelessWidget {
         id: '2', title: 'Conta de luz', amount: 77.89, date: DateTime.now()),
   ];
 
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,108 +37,116 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Colors.deepPurple,
         title: const Text('Gerenciador de gastos'),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(
-              width: double.infinity,
-              child: Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8))),
-                color: Colors.deepPurple,
-                child: SizedBox(
-                  width: 300,
-                  height: 100,
-                  child: Center(
-                    child: Text(
-                      'Lista de gastos',
-                      style: TextStyle(fontSize: 24, color: Colors.white),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(color: Colors.black12),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                width: double.infinity,
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8))),
+                  color: Colors.deepPurple,
+                  child: SizedBox(
+                    width: 300,
+                    height: 100,
+                    child: Center(
+                      child: Text(
+                        'Lista de gastos',
+                        style: TextStyle(fontSize: 24, color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Card(
-              elevation: 5,
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 16, right: 16),
-                    child: TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 16, right: 16),
-                    child: TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Add Transaction',
-                          style: TextStyle(color: Colors.deepPurple, fontSize: 16),
-                        )),
-                  ),
-                ],
-              ),
-            ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                  elevation: 5,
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8))),
-                  child: Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          color: Colors.deepPurple[400],
-                        ),
-                        width: 90,
-                        height: 85,
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'R\$${tx.amount}',
-                                style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18),
-                              ),
-                            ]),
+              Card(
+                elevation: 5,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16, right: 16),
+                      child: TextField(
+                        controller: titleController,
+                        decoration: const InputDecoration(labelText: 'Title'),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 8),
-                              child: Text(tx.title,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16, right: 16),
+                      child: TextField(
+                        controller: amountController,
+                        decoration: const InputDecoration(labelText: 'Amount'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextButton(
+                          onPressed: () {
+                            print(titleController.text);
+                            print(amountController.text);
+                          },
+                          child: const Text(
+                            'Add Transaction',
+                            style: TextStyle(color: Colors.deepPurple, fontSize: 16),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: transactions.map((tx) {
+                  return Card(
+                    elevation: 5,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    child: Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Colors.deepPurple[400],
+                          ),
+                          width: 90,
+                          height: 85,
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'R\$${tx.amount}',
                                   style: const TextStyle(
+                                      color: Colors.white,
                                       fontWeight: FontWeight.bold,
-                                      fontSize: 16)),
-                            ),
-                            Text(
-                              DateFormat().add_yMMMd().format(tx.date),
-                              style: const TextStyle(color: Colors.grey),
-                            ),
-                          ],
+                                      fontSize: 18),
+                                ),
+                              ]),
                         ),
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            )
-          ],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 8),
+                                child: Text(tx.title,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16)),
+                              ),
+                              Text(
+                                DateFormat().add_yMMMd().format(tx.date),
+                                style: const TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }).toList(),
+              )
+            ],
+          ),
         ),
       ),
     );
