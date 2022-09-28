58823bbe7825f84444aa3ffdf744cda5e0eaebd6
silveiraluisf <silveiraluis88@gmail.com>
2022-09-28 13:44:11 -0300

    trocado listview por listview.builder

--- lib/components/transactions_list.dart
+++ lib/components/transactions_list.dart
@@ -11,57 +11,60 @@ class TransactionsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 280,
-      child: ListView(
-      children: transactions.map((tx) {
-        return Card(
-          elevation: 3,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(8))),
-          child: Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(left: 5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.deepPurple[400],
-                ),
-                width: 90,
-                height: 50,
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Text(
-                    'R\$${tx.amount.toStringAsFixed(2)}',
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18),
+        height: 300,
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            // children: transactions.map((tx) {
+            return Card(
+              elevation: 3,
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8))),
+              child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.deepPurple[400],
+                    ),
+                    width: 90,
+                    height: 50,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'R\$${transactions[index].amount.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                        ]),
                   ),
-                ]),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8),
+                          child: Text(transactions[index].title,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16)),
+                        ),
+                        Text(
+                          DateFormat().add_yMMMd().format(transactions[index].date),
+                          style: const TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 8),
-                      child: Text(tx.title,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16)),
-                    ),
-                    Text(
-                      DateFormat().add_yMMMd().format(tx.date),
-                      style: const TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        );
-      }).toList(),
-    ),
+            );
+          },
+          itemCount: transactions.length,
+        ),
     );
   }
 }
