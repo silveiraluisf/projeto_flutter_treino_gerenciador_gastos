c796cd8b64b337197b709e95adc4139b40e9badd
silveiraluisf <silveiraluis88@gmail.com>
2022-09-28 17:20:14 -0300

    card do grafico já recebe informacoes de gastos por dia (sem formatacao)

---
+++ lib/components/chart_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(height: 4,),
+        Container(),
+      ],
+    );
+  }
+}
